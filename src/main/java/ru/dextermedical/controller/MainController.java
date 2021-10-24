8eee9f9e37e84e2ddd780683ee4d156b73072d9a
agent_tim <agent_tim@mail.ru>
2021-10-24 16:20:07 +0500

    02_24.10.21

--- src/main/java/ru/dextermedical/controller/MainController.java
+++ src/main/java/ru/dextermedical/controller/MainController.java
@@ -1,19 +1,17 @@
-package ru.dextermedical;
+package ru.dextermedical.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.dextermedical.domain.Message;
 import ru.dextermedical.repos.MessageRepo;
 
-import java.util.List;
 import java.util.Map;
 
 @Controller
-public class GreetingController {
+public class MainController {
     @Autowired
     private MessageRepo messageRepo;
 
---
+++ src/main/java/ru/dextermedical/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package ru.dextermedical.repos;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.dextermedical.domain.User;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
