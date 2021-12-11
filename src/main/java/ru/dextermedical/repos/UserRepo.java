501c055366d482b878707a3d3fea192ddd9d93e2
agent_tim <agent_tim@mail.ru>
2021-12-11 11:13:23 +0500

    01_11.12.21

--- src/main/java/ru/dextermedical/repos/UserRepo.java
+++ src/main/java/ru/dextermedical/repos/UserRepo.java
@@ -5,4 +5,6 @@ import ru.dextermedical.domain.User;
 
 public interface UserRepo extends JpaRepository<User, Long> {
     User findByUsername(String username);
+
+    User findByActivationCode(String code);
 }
